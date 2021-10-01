a2603a242391326bf06ff92a7e1ca6ef0686c9d1
phamtanphat <phatdroid94@gmail.com>
2021-10-01 21:24:05 +0700

    life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator_15092021/calculator_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CalculatorPage(),
     );
   }
 }
