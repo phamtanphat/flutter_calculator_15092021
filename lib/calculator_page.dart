52ce4b0c6e835476bf0e5d98c7c3eb8cb8720d3c
phamtanphat <phatdroid94@gmail.com>
2021-10-04 21:34:13 +0700

    update ui

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -24,91 +24,93 @@ class _CalculatorPageState extends State<CalculatorPage> {
       appBar: AppBar(
         title: Text("Calculator App"),
       ),
-      body: Container(
-          constraints: BoxConstraints.expand(),
-          child: Column(
-            children: [
-              Image.asset(AppImage.CALCULATE,
-                  width: width / 3, height: height / 5),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  cursorColor: Colors.red,
-                  maxLength: 4,
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.only(left: 10, top: 20, bottom: 20),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(5)),
-                        borderSide: BorderSide(width: 2)),
-                    hintText: "Input number 1",
-                    labelText: "Number 1",
+      body: SingleChildScrollView(
+        child: Container(
+            constraints: BoxConstraints(maxHeight: height),
+            child: Column(
+              children: [
+                Image.asset(AppImage.CALCULATE,
+                    width: width / 3, height: height / 5),
+                SizedBox(height: 5),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.red,
+                    maxLength: 4,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.only(left: 10, top: 20, bottom: 20),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide(width: 2)),
+                      hintText: "Input number 1",
+                      labelText: "Number 1",
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  cursorColor: Colors.red,
-                  maxLength: 4,
-                  decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.only(left: 10, top: 20, bottom: 20),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(5)),
-                        borderSide: BorderSide(width: 2)),
-                    hintText: "Input number 2",
-                    labelText: "Number 2",
+                SizedBox(height: 5),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.red,
+                    maxLength: 4,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.only(left: 10, top: 20, bottom: 20),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide(width: 2)),
+                      hintText: "Input number 2",
+                      labelText: "Number 2",
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Visibility(
-                  visible: true,
-                  child: Text("Kết quả = 10",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red))),
-              SizedBox(height: 20),
-              Expanded(
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: Row(
-                          children: [
-                            ElevatedButton(onPressed: (){}, child: Text("+" , style: TextStyle(fontSize: 20))),
-                            ElevatedButton(onPressed: (){}, child: Text("-" , style: TextStyle(fontSize: 20))),
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                SizedBox(height: 5),
+                Visibility(
+                    visible: true,
+                    child: Text("Kết quả = 10",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red))),
+                SizedBox(height: 5),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          child: Row(
+                            children: [
+                              ElevatedButton(onPressed: (){}, child: Text("+" , style: TextStyle(fontSize: 20))),
+                              ElevatedButton(onPressed: (){}, child: Text("-" , style: TextStyle(fontSize: 20))),
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Row(
-                          children: [
-                            ElevatedButton(onPressed: (){}, child: Text("*" , style: TextStyle(fontSize: 20))),
-                            ElevatedButton(onPressed: (){}, child: Text("/" , style: TextStyle(fontSize: 20))),
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                      Expanded(
+                        child: Container(
+                          child: Row(
+                            children: [
+                              ElevatedButton(onPressed: (){}, child: Text("*" , style: TextStyle(fontSize: 20))),
+                              ElevatedButton(onPressed: (){}, child: Text("/" , style: TextStyle(fontSize: 20))),
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-            ],
-          )),
+                SizedBox(height: 5),
+              ],
+            )),
+      ),
     );
   }
 }
